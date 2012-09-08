10b5ffdb07e5965558ce8ea4f7c566c61fc57adb
Martin Kielhorn <kielhorn.martin@googlemail.com>
2012-09-08 11:15:16 +0200

    Find out platform name.
    martin@acergpu:~/0908/opencl-test$ ./ocl-test
    platform 0 NVIDIA CUDA

--- ocl-test.c
+++ ocl-test.c
@@ -3,8 +3,22 @@
 int
 main()
 {
-  cl_uint NumPlatforms;
-  clGetPlatformIDs(0,NULL,&NumPlatforms);
-  printf("%d\n",NumPlatforms);
+  cl_uint n;
+  clGetPlatformIDs(0,NULL,&n);
+  
+  {
+    cl_platform_id p[n];
+    clGetPlatformIDs(n,p,NULL);
+    int i;
+    for(i=0;i<n;i++){
+      char buf[1024];
+      clGetPlatformInfo(p[i],CL_PLATFORM_NAME,sizeof(buf),buf,NULL);
+      printf("platform %d %s\n",i,buf);
+    }
+  }
+  
+
+
+
   return 0;
 }
